cc160fe1b6020cc715f8175e3f96b515bc60ccdb
schmooze123 <39678145+schmooze123@users.noreply.github.com>
2019-01-02 18:45:47 +0530

    Update settings.py

--- blushbyanshii/settings.py
+++ blushbyanshii/settings.py
@@ -144,16 +144,16 @@ LOGOUT_URL = 'logout_user'
 LOGIN_REDIRECT_URL = 'index'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+NEW_BASE_DIR = BASE_DIR+'/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn', 'static_root')
+STATIC_ROOT = os.path.join(os.path.dirname(NEW_BASE_DIR), 'static_cdn', 'static_root')
 print (STATIC_ROOT)
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn', 'media_root')
+MEDIA_ROOT = os.path.join(os.path.dirname(NEW_BASE_DIR), 'static_cdn', 'media_root')
 
 # Stripe Settings
 
