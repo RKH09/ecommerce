4e332ec72a351f09c622b7645b547ec9789731fb
schmooze123 <39678145+schmooze123@users.noreply.github.com>
2019-01-02 18:57:28 +0530

    Update settings.py

--- blushbyanshii/settings.py
+++ blushbyanshii/settings.py
@@ -148,6 +148,7 @@ LOGIN_REDIRECT_URL = 'index'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static_cdn', 'static_root')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn', 'static_root')
 print (STATIC_ROOT)
