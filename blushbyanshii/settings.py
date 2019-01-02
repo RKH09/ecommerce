2be0f6f17eeeae36fb330123f99c771ee141c653
schmooze123 <39678145+schmooze123@users.noreply.github.com>
2019-01-02 19:03:52 +0530

    Update settings.py

--- blushbyanshii/settings.py
+++ blushbyanshii/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print (BASE_DIR)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -146,12 +146,11 @@ LOGIN_REDIRECT_URL = 'index'
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 #NEW_BASE_DIR = BASE_DIR+'/'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, 'static_cdn', 'static_root')
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "static"),
+#    os.path.join(BASE_DIR, 'static_cdn', 'static_root')
+#]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn', 'static_root')
-print (STATIC_ROOT)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static_cdn', 'media_root')
