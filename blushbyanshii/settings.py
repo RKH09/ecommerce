bd9ffe707183158624fd5b0c0a19cd3733eadc83
schmooze123 <39678145+schmooze123@users.noreply.github.com>
2019-01-02 18:39:32 +0530

    Update settings.py

--- blushbyanshii/settings.py
+++ blushbyanshii/settings.py
@@ -150,7 +150,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn', 'static_root')
-
+print (STATIC_ROOT)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn', 'media_root')
